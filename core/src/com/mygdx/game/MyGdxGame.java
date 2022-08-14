ab352318cae6dfaff2eff0b71826035a0b2d87c8
olga_komar <komar_ol@mail.ru>
2022-08-14 18:03:07 +0300

    lesson 1(game LibGDX)

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+
+  SpriteBatch batch;
+  Texture img;
+  float x;
+  float y;
+
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+    img = new Texture("games_hero.png");
+    x = Gdx.graphics.getWidth() / 2 - img.getWidth() / 2;
+    y = Gdx.graphics.getHeight() / 2 - img.getHeight() / 2;
+
+  }
+
+  @Override
+  public void render() {
+    ScreenUtils.clear(1, 1, 1, 1);
+
+     x =  Gdx.input.getX() - img.getWidth() / 2;
+     y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight() / 2;
+
+    batch.begin();
+    batch.draw(img, x, y);
+    batch.end();
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+    img.dispose();
+  }
+}
